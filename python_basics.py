b1cba7b64b773583a7d3e324ac1c42a668951689
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-18 18:49:04 -0500

    added python_basics.py

---
+++ python_basics.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+print("\nPython Basics")
+
+print("\nData Types")
+
+print("\nInteger Operations")
+
+num1 = 5
+num2 = 4
+print("Num1 = {0}, Num2 = {1}".format(num1, num2))
+
+# add
+add = num1 + num2
+print("{0} + {1} = {2}".format(num1, num2, add))
+
+# sub
+sub = num1 - num2
+print("{0} - {1} = {2}".format(num1, num2, sub))
+
+# multiplication
+mul = num1 * num2
+print("{0} * {1} = {2}".format(num1, num2, mul))
+
+# division
+div = num1 / num2
+print("{0} / {1} = {2}".format(num1, num2, div))
+
+# modulus
+mod = num1 % num2
+print("{0} % {1} = {2}".format(num1, num2, mod))
+
+number = 1
+name = "Saul"
+
+print(str(number) + name)
+
+frac = 3.2456
+
+print("frac = {0}, rounded = {1}".format(frac, round(frac, 2)))
